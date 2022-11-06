09afde982fc56ab198c5e8d92304200d94a8f2f6
NgocDuc82 <nguyenngocduc0802@gmail.com>
2022-11-06 21:14:43 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "./GlobalStyle/GlobalStye";
+// import GlobalStyle from "./GlobalStyle/GlobalStye";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle>
+
       <App />
-    </GlobalStyle>
+
   </React.StrictMode>
 );
 
