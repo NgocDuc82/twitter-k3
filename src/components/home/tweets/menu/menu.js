94e5d0a6022d07c29646f5614bdeac891722ba23
Lã Phúc <phuclv@reactplus.jp>
2022-11-07 17:05:55 +0700

    update

--- src/components/home/tweets/menu/menu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-export default function menu() {
-  return (
-    <div className="menu">
-        <MoreHorizIcon />
-    </div>
-  )
-}
