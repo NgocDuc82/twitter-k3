0f6d630abc7c777a2be55ac4e9a6fc25a3e98fc7
thanhlong1703 <phamthanhlong173@gmail.com>
2022-11-10 11:49:49 +0700

    devlong

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,22 +1,22 @@
-import classNames from 'classnames/bind'
-import { Outlet } from 'react-router-dom'
+import classNames from "classnames/bind";
+import { Outlet } from "react-router-dom";
 
-import styles from './DefaultLayout.module.scss'
-import Navbar from '../../components/home/navbar/Navbar'
+import styles from "./DefaultLayout.module.scss";
+import Navbar from "../../components/home/navbar/navbar";
 
-const DefaultLayout = ({children}) => {
-    const cx = classNames.bind(styles)
+const DefaultLayout = ({ children }) => {
+  const cx = classNames.bind(styles);
 
-    return (
-        <div className={cx("container")}>
-            <div className={cx("sidebar")}>
-                <Navbar />
-            </div>
-            <div className={cx("content")}>
-                <Outlet/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={cx("container")}>
+      <div className={cx("sidebar")}>
+        <Navbar />
+      </div>
+      <div className={cx("content")}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default DefaultLayout;
